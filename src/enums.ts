bec6c6f58a5c6699069e38a5aac440892ad0c017
hashan <hashan@mainframe.lk>
2025-08-21 13:20:16 +0530

    resolved file structure

--- src/enums.ts
+++ src/enums.ts
@@ -1,16 +1,15 @@
-export const PRIORITY = {
-  HIGH: 1,
-  LOW: 0,
-} as const;
+export enum PRIORITY {
+  LOW = 0,
+  HIGH = 1,
+}
 
-export const ALERT_TYPE = {
-  ERROR: "ERROR",
-  WARNING: "WARNING",
-  SUCCESS: "SUCCESS",
-  INFO: "INFO",
-  NOTICE: "NOTICE",
-  QUESTION: "QUESTION",
-  LOADING: "LOADING",
-  TIP: "TIP",
-} as const;
-
+export enum ALERT_TYPE {
+  ERROR = "ERROR",
+  WARNING = "WARNING",
+  SUCCESS = "SUCCESS",
+  INFO = "INFO",
+  NOTICE = "NOTICE",
+  QUESTION = "QUESTION",
+  LOADING = "LOADING",
+  TIP = "TIP",
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { ALERT_TYPE, PRIORITY } from './enums';
+export type {
+    AlertButton,
+    AlertData,
+    AlertModalProps,
+    AlertModalRef,
+    Priority,
+    GlobalAlertInstance,
+    GlobalAlertProviderProps
+} from './types';
+export { AlertModalWrapper } from './AlertModalWrapper';
+export { GlobalAlertProvider } from './GlobalAlertProvider';
+export { GlobalAlert, createGlobalAlert } from './globalAlert';
+export { GlobalAlertContext, useGlobalAlert } from './useGlobalAlert';
